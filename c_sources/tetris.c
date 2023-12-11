f93a86a2cd6c56d333e46d0d3f54271eff31e6c2
Royialchen <cry@LAPTOP-7E9FB7PU.localdomain>
2023-12-11 19:42:21 +0800

    修复俄罗斯方块的bug具有更好游戏体验

--- c_sources/tetris.c
+++ c_sources/tetris.c
@@ -8,8 +8,8 @@
 #include "sys.h"
 
 #define Height 28
-#define Width 40
-#define Py_x 10
+#define Width 30
+#define Py_x 10 
 #define Py_y 1
 // 定义一个 10行 15列 的游戏区域
 int game_area[Height][Width] = { 0 };
@@ -110,7 +110,7 @@ void clear_lines() {
 // 生成新的方块函数
 void spawn() {
     block_type = Qrand() % 7;
-    block_x_pos = 6;
+    block_x_pos = Width / 2 - 1;
     block_y_pos = 0;
     block_rotation = 0;
     block_color = Qrand()%8;
@@ -162,26 +162,26 @@ void print_game_area() {
     char sco_s = score / 10 + '0';
     if(sco_s == '0')sco_s=' ';
     char sco_g = score % 10 + '0';
-    displayChar(1 + Width + Py_x + 5, 10, 'S', WHITE);
-    displayChar(2 + Width + Py_x + 5, 10, 'c', WHITE);
-    displayChar(3 + Width + Py_x + 5, 10, 'o', WHITE);
-    displayChar(4 + Width + Py_x + 5, 10, 'r', WHITE);
-    displayChar(5 + Width + Py_x + 5, 10, 'e', WHITE);
-    displayChar(6 + Width + Py_x + 5, 10, ':', WHITE);
-    displayChar(7 + Width + Py_x + 5, 10, sco_s, WHITE);
-    displayChar(8 + Width + Py_x + 5, 10, sco_g, WHITE);
+    displayChar(1 + Width + Py_x + 5, 14, 'S', WHITE);
+    displayChar(2 + Width + Py_x + 5, 14, 'c', WHITE);
+    displayChar(3 + Width + Py_x + 5, 14, 'o', WHITE);
+    displayChar(4 + Width + Py_x + 5, 14, 'r', WHITE);
+    displayChar(5 + Width + Py_x + 5, 14, 'e', WHITE);
+    displayChar(6 + Width + Py_x + 5, 14, ':', WHITE);
+    displayChar(7 + Width + Py_x + 5, 14, sco_s, WHITE);
+    displayChar(8 + Width + Py_x + 5, 14, sco_g, WHITE);
 
     char levl_s = level / 10 + '0';
     if(levl_s == '0')levl_s=' ';
     char levl_g = level % 10 + '0';
-    displayChar(1 + Width + Py_x + 5, 13, 'L', WHITE);
-    displayChar(2 + Width + Py_x + 5, 13, 'e', WHITE);
-    displayChar(3 + Width + Py_x + 5, 13, 'v', WHITE);
-    displayChar(4 + Width + Py_x + 5, 13, 'e', WHITE);
-    displayChar(5 + Width + Py_x + 5, 13, 'l', WHITE);
-    displayChar(6 + Width + Py_x + 5, 13, ':', WHITE);
-    displayChar(7 + Width + Py_x + 5, 13, levl_s, WHITE);
-    displayChar(8 + Width + Py_x + 5, 13, levl_g, WHITE);
+    displayChar(1 + Width + Py_x + 5, 16, 'L', WHITE);
+    displayChar(2 + Width + Py_x + 5, 16, 'e', WHITE);
+    displayChar(3 + Width + Py_x + 5, 16, 'v', WHITE);
+    displayChar(4 + Width + Py_x + 5, 16, 'e', WHITE);
+    displayChar(5 + Width + Py_x + 5, 16, 'l', WHITE);
+    displayChar(6 + Width + Py_x + 5, 16, ':', WHITE);
+    displayChar(7 + Width + Py_x + 5, 16, levl_s, WHITE);
+    displayChar(8 + Width + Py_x + 5, 16, levl_g, WHITE);
     //printf("Score: %d\n", score);
     //printf("Level: %d\n", level);
 }
@@ -195,7 +195,7 @@ void tetris_exec() {
         }
     }
     // 生成新的方块
-    Qsrand(0);
+    Qsrand(time(0));
     spawn();
     while (1) {
         // 更新游戏区域
@@ -203,7 +203,7 @@ void tetris_exec() {
         // 显示游戏界面
         print_game_area();
         // 延迟一段时间，实现动态效果
-        sleep(speed);
+        //sleep(speed);
         // 清除当前方块
         int x, y, new_x, new_y;
         for (int i = 0; i < 4; i++) {
@@ -228,6 +228,9 @@ void tetris_exec() {
             update_game_area();
             clear_lines();
             spawn();
+            if (!can_move(0, 0, block_rotation)) {
+                break;
+            }
             // 检查游戏是否结束（即游戏区域顶部有方块）
             int game_over = 0;
             for (int i = 0; i < Width; i++) {
@@ -241,8 +244,9 @@ void tetris_exec() {
             }
         }
         // 处理用户输入
-        if (kbhit()) {
-            char input = getchar();
+        char fget = kbhit(speed,0);
+        if(fget != 0){
+            char input = fget;
             switch (input) {
             case 'W': case 'w': move(0, 0, block_rotation + 1); break;
             case 'A': case 'a': move(-1, 0, block_rotation); break;
@@ -252,6 +256,17 @@ void tetris_exec() {
             default: break;
             }
         }
+        // if (kbhit()) {
+        //     char input = getchar();
+        //     switch (input) {
+        //     case 'W': case 'w': move(0, 0, block_rotation + 1); break;
+        //     case 'A': case 'a': move(-1, 0, block_rotation); break;
+        //     case 'S': case 's': move(0, 1, block_rotation); break;
+        //     case 'D': case 'd': move(1, 0, block_rotation); break;
+        //     case 'Q': case 'q': move(0, 0, block_rotation - 1); break;
+        //     default: break;
+        //     }
+        // }
         
     }
     // 游戏结束，显示最终得分
